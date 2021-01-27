c0b2e65d37349e7a5095ccee66195f8fe3c914f4
Lechoslaw Radwanski <lupercaldev@gmail.com>
2021-01-27 16:51:52 +0100

    add resume template and view

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-from models import Resume
+from django.views import View
+
+from resume.models import Resume
 # Create your views here.
+
+class ResumeView(View):
+    def get(self, request, *args, **kwargs):
+        resumes = Resume.objects.all()
+        print("Asdasdas")
+        return render(request, "resume/resumes.html",
+                      context={"resumes": resumes})
