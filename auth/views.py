bfebb3c616817db00a11bde310f6636de8025800
Lechoslaw Radwanski <lupercaldev@gmail.com>
2021-01-28 14:22:33 +0100

    add login view

--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 from django.views.generic import CreateView
@@ -9,3 +9,9 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = "/login"
     template_name = "signup.html"
+
+
+class SignInView(LoginView):
+    redirect_authenticated_user = True
+    template_name = "login.html"
+
