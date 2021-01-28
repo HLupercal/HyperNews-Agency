1073ae06b2455d2ad2ff8e761391b2480b8fdfc9
Lechoslaw Radwanski <lupercaldev@gmail.com>
2021-01-28 14:43:53 +0100

    add hyperjobauth templates urls and app to installed apps

--- hyperjob/settings.py
+++ hyperjob/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'hyperjob.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'hyperjobauth/templates/hyperjobauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
