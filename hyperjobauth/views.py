296b17c56c77e7eb4fd70ad38210a74ca661ff93
Lechoslaw Radwanski <lupercaldev@gmail.com>
2021-01-28 14:32:59 +0100

    add auth views and templates

--- hyperjobauth/views.py
+++ hyperjobauth/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = "/login"
+    template_name = "signup.html"
+
+
+class SignInView(LoginView):
+    redirect_authenticated_user = True
+    template_name = "login.html"
+
